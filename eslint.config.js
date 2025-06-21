83397f0c4a9ea1035bc29ccec1b140d8fb719eed
Wim Bonis <wb@stylite.de>
2025-06-21 23:16:30 +0200

    Add Eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  js.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  { ...pluginReact.configs.flat.recommended, settings: { react: { version: "detect" } } },
+];
