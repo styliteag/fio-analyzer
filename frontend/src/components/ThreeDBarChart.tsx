59b502b95bffd4f39897ea2436b1650cd609d88e
Wim Bonis <wb@stylite.de>
2025-07-05 00:40:02 +0200

    Enable unknown property handling in ThreeDBarChart component by disabling eslint rule for unknown properties. This change allows for greater flexibility in using custom properties within the component.

--- frontend/src/components/ThreeDBarChart.tsx
+++ frontend/src/components/ThreeDBarChart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Text, Grid } from '@react-three/drei';
